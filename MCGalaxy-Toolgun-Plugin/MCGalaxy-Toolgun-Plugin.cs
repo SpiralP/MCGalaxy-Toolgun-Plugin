bb14820b853c4173131671f1ecbd494ecd272d44
SpiralP <spiral.palmer@gmail.com>
2024-09-17 17:59:50 -0700

    init

---
+++ MCGalaxy-Toolgun-Plugin/MCGalaxy-Toolgun-Plugin.cs
@@ -0,0 +1,9 @@
+﻿namespace MCGalaxy
+{
+    public sealed partial class MCGalaxyToolgunPlugin : Plugin
+    {
+        public override string name { get { return "Toolgun"; } }
+        public override void Load(bool startup) { }
+        public override void Unload(bool shutdown) { }
+    }
+}
